d8a11e21dc808b154a7a96eaf6c8c7cb63c8a851
Djhesica <dhessycacortez2014@gmail.com>
2020-04-04 14:50:46 -0300

    Aula Bloco de Notas

---
+++ app/src/main/java/com/example/constraitlayout/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.constraitlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
+public class MainActivity extends AppCompatActivity {
+    Notas notas;
+    EditText editText;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.editText = findViewById(R.id.editText);
+        notas = new Notas(this.getApplicationContext());
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                notas.salvaNota(editText.getText().toString());
+            }
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        editText.setText(notas.recuperaNota());
+    }
+}
